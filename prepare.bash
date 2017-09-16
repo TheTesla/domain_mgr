508241fd9bf6f0dd0eb6ce0608e889defc05e9ee
root <root@testserver.smartrns.net>
2017-09-16 17:15:48 +0000

    SAN certificate generation

--- prepare.bash
+++ prepare.bash
@@ -4,14 +4,22 @@ source domain_config.bash
 
 
 systemctl stop apache2
+systemctl stop nginx
+
+combineddomains=""
 for domain in $domainlist
 do
   echo ""
   echo "---- renew: $domain ----"
   echo ""
-  ./certbot/certbot-auto certonly --email "$certemail" $certextra --agree-tos --non-interactive --standalone --rsa-key-size 4096 -d "$domain"
+  combineddomains=" $combineddomains -d $domain"
+  #./certbot/certbot-auto certonly --email "$certemail" $certextra --agree-tos --non-interactive --standalone --rsa-key-size 4096 -d "$domain"
 done
+echo "$combineddomains"
+./certbot/certbot-auto certonly --email "$certemail" $certextra --agree-tos --non-interactive --standalone --expand --rsa-key-size 4096 $combineddomains
+
 systemctl start apache2
+systemctl start nginx
 
 echo ""
 echo "---- update all tlsa entries ----"
--- rollover.bash
+++ rollover.bash
@@ -6,9 +6,18 @@ source domain_config.bash
 src=$certbasepath
 dest=$certdest 
 
-cp -rfL $src/* $dest/.
+for primdomain in $domainlist
+do
+  break
+done
 
+for domain in $domainlist
+do
+  echo $src/$primdomain
+  cp -rfL $src/$primdomain $dest/$domain
+done 
 systemctl restart apache2
+systemctl restart nginx
 
 ./delete_tlsa.bash
 
