6300e156dacfed6bebdbeeb13472d95e5a432348
root <root@testserver.smartrns.net>
2017-09-18 01:47:45 +0000

    added option to dkim_signing.conf to allow subdomain dkim keys

--- dkim_signing_template.bash
+++ dkim_signing_template.bash
@@ -1,5 +1,6 @@
 echo 'path = "/var/lib/rspamd/dkim/$selector.key";'
 echo "selector = \"$1\";"
+echo 'use_esld = false'
 echo ''
 echo '### Enable DKIM signing for alias sender addresses'
 echo 'allow_username_mismatch = true;'
