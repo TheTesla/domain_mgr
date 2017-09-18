1403fc94c5f5379daaee6c68d46ebc3d0e37b7bd
root <root@testserver.smartrns.net>
2017-09-18 01:28:39 +0000

    bugfix: _keyname.domainkey.domain is wrong, it has to be wothout _ - ToDo: find out why dkimvalidator wants key in domain but not in correct subdomain

--- inwx_add_dkim.bash
+++ inwx_add_dkim.bash
@@ -2,7 +2,7 @@
 
 # inwx_add_dkim.bash inwxapi inwxlogin inwxpasswd name tld entry keyname ttl
 
-python ./python2.7-client/update_ns_record.py "$1" "$2" "$3" "add" "_$7._domainkey.$4" "$5" "TXT" "$6" $8
+python ./python2.7-client/update_ns_record.py "$1" "$2" "$3" "add" "$7._domainkey.$4" "$5" "TXT" "$6" $8
 
 
 
